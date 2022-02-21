e4e60cbb10475358f6d9bfea3fa975888d3e2bc7
luiseer <85654418+luiseer@users.noreply.github.com>
2022-02-20 22:22:26 -0600

    node modules

--- app/src/index.js
+++ app/src/index.js
@@ -32,34 +32,37 @@ export const Total = ({ course }) => {
   )
 }
 
+
+
 const App = (props) => {
 
-  const course = {
+ 
+   const course = {
 
     name: 'Half Stack application development',
 
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
+   parts: [
+     {
+       name: 'Fundamentals of React',
+       exercises: 10
+     },
+     {
+       name: 'Using props to pass data',
+       exercises: 7
+     },
+     {
+       name: 'State of a component',
+       exercises: 14
+     }
+   ]
+ }
+ return (
+   <div>
+     <Header course={course} />
+     <Content course={course} />
+     <Total course={course} />
+   </div>
+ )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
