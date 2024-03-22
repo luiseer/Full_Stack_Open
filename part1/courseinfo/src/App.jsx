cf598ad597b8c2d4bafb6ee4dba5a94453ae2eca
luiseer <85654418+luiseer@users.noreply.github.com>
2024-03-21 21:23:14 -0600

    exercises 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -4,7 +4,9 @@ const Title = ({course}) => <h1>{course}</h1>
 const Content = (props) =>{
   return(
     <div>
-      <p>{props.part} exercises {props.exercises}</p>
+      <Part/>
+      <Part/>
+      <Part/>
     </div>
   )
 }
@@ -17,10 +19,20 @@ const Total = (props) =>{
   )
 }
 
-const App = () => {
-  const course = 'Half Stack application development'
+const Part = (props) =>{
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
+  return(
+    <div>
+      <p>{part1} and {exercises1}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const course = 'Half Stack application development'
   const part2 = 'Using props to pass data'
   const exercises2 = 7
   const part3 = 'State of a component'
@@ -28,23 +40,7 @@ const App = () => {
 
   return (
     <div>
-      <Title course={course}/>
-      <Content 
-        part={part1} 
-        exercises={exercises1}
-        />
-      <Content 
-        part={part2}
-        exercises={exercises2}
-        />
-      <Content 
-        part={part3}
-        exercises={exercises3}
-        />
-      
-      <Total
-        total={exercises1+exercises2+exercises3}
-        />
+      <Content/>
     </div>
   )
 }
