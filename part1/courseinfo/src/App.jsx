c6189157b2031e6c98e7fef587830ed36accaec8
luiseer <85654418+luiseer@users.noreply.github.com>
2024-05-07 20:52:11 -0600

    exercise 1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,28 @@
 const App = () => {
-  const course = 'Half Stack aplication developemt'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+  console.log(course.parts[0].name);
   return(
     <div>
-      <h1>{course}</h1>
-      <p>{parts[0].name}, excercises {parts[0].exercises}</p>
-      <p>{parts[1].name}, excercises {parts[1].exercises}</p>
-      <p>{parts[2].name}, excercises {parts[2].exercises}</p>
+      <h1>{course.name}</h1>
+      <p>{course.parts[0].name}, {course.parts[0].exercises}</p>
+      <p>{course.parts[1].name}, {course.parts[1].exercises}</p>
+      <p>{course.parts[2].name}, {course.parts[2].exercises}</p>
     </div>
   )
 }
