1b6008455291d233af383f9812f833e3fd802f3c
luiseer <85654418+luiseer@users.noreply.github.com>
2024-06-11 16:23:12 -0600

    update

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,34 +1,69 @@
 import { useState } from "react"
+
+const History = (props) => {
+  if (props.leftOrRigthClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+  return (
+    <div>
+      button press history: {props.leftOrRigthClicks.join(' ')}
+    </div>
+  )
+}
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
   
 
 const App = () => {
 
   const [clicks, setClicks] = useState(
-    {left: 0,
-      rigth: 0
+    {
+      left: 0,
+      rigth: 0,
+      totalClicks: 0,
+      leftOrRigthClicks: [] 
     }
   )
 
+  console.log(clicks);
+
   const handleLeftClick = () => {
-    const newClicks = {
+    setClicks({
       ...clicks,
-      left: clicks.left + 1
-    }
-    setClicks(newClicks)
+      totalClicks: clicks.totalClicks + 1,
+      leftOrRigthClicks: clicks.leftOrRigthClicks.concat('L'),
+      left: clicks.left + 1})
   }
   const handleRigthClick = () => {
-    const newClicks = {
+    setClicks({
       ...clicks,
-      rigth: clicks.rigth +1
-    }
-    setClicks(newClicks)
+      totalClicks: clicks.totalClicks + 1,
+      leftOrRigthClicks: clicks.leftOrRigthClicks.concat('R'),
+      rigth: clicks.rigth + 1})
+  }
+  const handleClicks = () =>{
+    setClicks({
+      ...clicks,
+      totalClicks: clicks.totalClicks +1
+    })
   }
   return(
     <div>
       {clicks.left}
-      <button onClick={handleLeftClick}>left</button>
-      <button onClick={handleRigthClick}>rigth</button>
+      <Button handleClick={handleLeftClick} text='left' />
+      <Button handleClick={handleRigthClick} text='right' />
       {clicks.rigth}
+      {<div onClick={handleClicks}>total: {clicks.totalClicks}</div>}
+      <History leftOrRigthClicks={clicks.leftOrRigthClicks}/>
+      {/* <p>{clicks.leftOrRigthClicks.join(' ')}</p> */}
     </div>
   )
 }
