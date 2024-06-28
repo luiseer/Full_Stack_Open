54fe9e7f8a9352e2da74aaddc467c526c12a0757
luiseer <85654418+luiseer@users.noreply.github.com>
2024-06-27 23:04:14 -0600

    exercises 1.7

--- part1/unicafe/app/src/App.jsx
+++ part1/unicafe/app/src/App.jsx
@@ -2,32 +2,59 @@ import { useState } from 'react'
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [feedBack, setFeedBack] = useState(
+    {
+      good: 0,
+      neutral: 0,
+      bad: 0,
+      total: 0,
+      average: [],
+      positive: 0
+    }
+  );
 
-  const handleGood = () => {
-    setGood(good + 1)
+  console.log(feedBack)
+
+  const handleGood = () =>{
+    setFeedBack({
+      ...feedBack,
+      total: feedBack.total + 1,
+      good: feedBack.good + 1,
+      positive: feedBack.total / 3
+    })
+  }
+  const handleNeutral = () =>{
+    setFeedBack({
+      ...feedBack,
+      total: feedBack.total + 1,
+      neutral: feedBack.neutral + 1,
+      positive: feedBack.total / 3
+    })
+  }
+  const handleBad = () =>{
+    setFeedBack({
+      ...feedBack,
+      total: feedBack.total + 1,
+      bad: feedBack.bad + 1,
+      positive: feedBack.total / 3
+    })
   }
 
-  const handleneutral = () => {
-    setNeutral(neutral + 1)
-  }
 
-  const handlebad = () => {
-    setBad( bad + 1)
-  }
 
   return (
     <div>
       <h1>give feedback</h1>
       <button onClick={handleGood}>good</button>
-      <button onClick={handleneutral}>neutral</button>
-      <button onClick={handlebad}>bad</button>
+      <button onClick={handleNeutral}>neutral</button>
+      <button onClick={handleBad}>bad</button>
       <h2>statics</h2>
-      <p>good: {good}</p>
-      <p>neutral: {neutral}</p>
-      <p>bad: {bad}</p>
+      <p>Good: {feedBack.good}</p>
+      <p>Neutral: {feedBack.neutral}</p>
+      <p>Bad: {feedBack.bad}</p>
+      <p>All {feedBack.total}</p>
+      <p>Average {feedBack.positive}</p>
+    
     </div>
   )
 }
