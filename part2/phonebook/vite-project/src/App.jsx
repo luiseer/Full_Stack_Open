222e00f73e964543c0ec7b09a414ca9df0a6ad58
luiseer <85654418+luiseer@users.noreply.github.com>
2024-09-04 18:03:20 -0600

    [UPDATE] Exercise 2.7

--- part2/phonebook/vite-project/src/App.jsx
+++ part2/phonebook/vite-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState } from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -7,28 +7,34 @@ const App = () => {
       name: 'Arto Hellas',
       // tel: '5544332211'
     }
-  ])
-  const [newName, setNewName] = useState()
-  // const [newTell, setnewTell] = useState('');
+  ]);
+  const [newName, setNewName] = useState('');
+  // const [newTell, setNewTell] = useState('');
 
   const handleAddName = (event) => {
-    console.log(event.target.value)
-    setNewName(event.target.value)
-  }
+    setNewName(event.target.value);
+  };
 
   const addContact = (event) => {
-    event.preventDefault()
-    const personObject = {
-      id: persons.length + 1,
-      name: newName
-    }
-    setPersons(persons.concat(personObject))
-    setNewName('')
-    
-  }
-  
+    event.preventDefault();
 
-  return(
+    // Verificar si el nombre ya está en la lista
+    const nameExists = persons.some(person => person.name === newName);
+
+    if (nameExists) {
+      alert('El nombre ya está en la lista');
+      setNewName(''); // Limpiar el campo de texto después de agregar
+    } else {
+      const personObject = {
+        id: persons.length + 1,
+        name: newName
+      };
+      setPersons(persons.concat(personObject));
+      setNewName(''); // Limpiar el campo de texto después de agregar
+    }
+  };
+
+  return (
     <>
       <h2>Phonebook</h2>
       <form onSubmit={addContact}>
@@ -37,7 +43,8 @@ const App = () => {
             placeholder='name'
             type='text'
             value={newName}
-            onChange={handleAddName}/>
+            onChange={handleAddName}
+          />
           {/* tel: <input type="text" /> */}
         </div>
         <div>
@@ -46,14 +53,12 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {
-          persons.map(person =>
-            <li key={person.id}>{person.name}</li>
-          )
-        }
+        {persons.map(person =>
+          <li key={person.id}>{person.name}</li>
+        )}
       </ul>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
