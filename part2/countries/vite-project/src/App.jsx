30b5e18efcda1b39908bedc53e27eb89cbbf42d2
unknown <85654418+luiseer@users.noreply.github.com>
2024-10-02 23:13:16 -0600

    [UPDATE] Exersise 2.18*

--- part2/countries/vite-project/src/App.jsx
+++ part2/countries/vite-project/src/App.jsx
@@ -3,31 +3,37 @@ import SearchBox from './components/SearchBox'
 import axios from 'axios'
 
 function App() {
-  const [countries, setCountries] = useState('')
+  const [countries, setCountries] = useState([])
+  const [findCountrie, setFindCountrie] = useState('')
 
-  const handleInput = (e) =>{
-    setCountries(e.target.value)
-  }
+  useEffect(() => {
+    axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
+      .then(response => setCountries(response.data))
+  }, [])
 
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    axios
-      .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${countries}`)
-      .then(response => console.log(response.data))
-    }
-    
+  const handleFind = (event) => setFindCountrie(event.target.value)
+
+  // Ahora filtramos por el nombre del país (estado) en lugar de la capital
+  const filterCountries = countries.filter(countrie => 
+    countrie.name.common.toLowerCase().includes(findCountrie.toLowerCase())
+  )
+
+  console.log(filterCountries.length)
   
-
 
   return (
     <>
       <h1>Countries</h1>
-      <SearchBox handleInput={handleInput} handleSubmit={handleSubmit}/>
-
+      <SearchBox  
+        handleFind={handleFind} 
+        findCountrie={findCountrie}
+        filterCountries={filterCountries}
+      />
     </>
   )
 }
 
 export default App
+
 
 
--- part2/countries/vite-project/src/components/SearchBox.jsx
+++ part2/countries/vite-project/src/components/SearchBox.jsx
@@ -1,17 +1,24 @@
+const SearchBox = ({ handleFind, findCountrie, filterCountries }) => {
+  return (
+    <>
+      <input 
+        type="text" 
+        onChange={handleFind} 
+        value={findCountrie} 
+        placeholder="Search by country"
+      />
+      
+      <ul>
+        {filterCountries.length > 0 ? (
+          filterCountries.map((countrie, index) => (
+            <li key={index}>{countrie.name.common}</li>
+          ))
+        ) : (
+          <li>No results found</li>
+        )}
+      </ul>
+    </>
+  )
+}
 
-const SearchBox = ({handleSubmit, handleInput}) => {
-
-
-  
-    return (
-      <>
-        <form onSubmit={handleSubmit}>
-          <input type="text" onChange={handleInput} />
-          <button>search</button>
-        </form>
-      </>
-    );
-  };
-  
-  export default SearchBox;
-  
+export default SearchBox
