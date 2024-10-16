7ab8918ae015b863685169b4361d29d57799ab7e
luiseer <85654418+luiseer@users.noreply.github.com>
2024-10-15 18:28:20 -0600

    [EXERSISE] 3.9

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
-
+app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
 morgan.token('body', (req) => {
   return JSON.stringify(req.body);
 });
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
-
 
 persons = 
 [
